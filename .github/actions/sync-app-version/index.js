bdb0db935664cbcb91a0ccf9ff56a5b45d35cef9
Vitalii Motso <vitalii.motso@gmail.com>
2023-09-12 15:54:24 +0300

    GH Action: sync-app-version
    Sync agent-helm release with the agent one
    SCALRCORE-27249

---
+++ .github/actions/sync-app-version/index.js
@@ -0,0 +1,75 @@
+const yaml = require('js-yaml')
+const core = require('@actions/core')
+const exec = require('@actions/exec')
+const github = require('@actions/github')
+const semver = require('semver')
+const fs = require('fs')
+const path = require('path')
+
+const chartsDir = path.join(process.env.GITHUB_WORKSPACE, 'charts')
+const appVersion = core.getInput('app_version', { required: true })
+const upstream = core.getInput('upstream', { required: true })
+
+function getCharts () {
+  const files = fs.readdirSync(chartsDir)
+  const directories = files.filter((file) => {
+    const filePath = path.join(chartsDir, file)
+    return fs.statSync(filePath).isDirectory()
+  })
+  core.debug(`Charts: ${directories}`)
+  return directories
+}
+
+function updateCharts () {
+  const charts = getCharts()
+  core.info(`The appVersion ${appVersion}`)
+  charts.forEach(function (chart) {
+    const chartPath = path.join(chartsDir, chart, 'Chart.yaml')
+    const chartData = yaml.load(fs.readFileSync(chartPath, 'utf8'))
+
+    chartData.appVersion = appVersion
+    chartData.version = semver.inc(chartData.version, 'patch')
+    const updatedYaml = yaml.dump(chartData)
+    fs.writeFileSync(chartPath, updatedYaml, 'utf8')
+    core.info(`The new version of ${chart} is ${chartData.version}`)
+  })
+}
+
+async function pushChanges () {
+  await exec.exec('git config user.name "github-actions[bot]"')
+  await exec.exec('git config user.email "github-actions[bot]@users.noreply.github.com"')
+  await exec.exec(`git checkout -b ${process.env.PR_BRANCH}`)
+  await exec.exec('git add charts')
+  await exec.exec(`git commit -m "Sync appVersion: ${appVersion}. Triggered by ${upstream}"`)
+  await exec.exec(`git push origin ${process.env.PR_BRANCH} --force`)
+}
+
+async function draftPR () {
+  try {
+    const octokit = github.getOctokit(process.env.GH_TOKEN)
+    const createResponse = await octokit.rest.pulls.create({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      title: `Sync appVersion ${appVersion} triggered by ${upstream}`,
+      head: process.env.PR_BRANCH,
+      base: process.env.GITHUB_REF_NAME
+    });
+    core.notice(
+      `Created PR #${createResponse.data.number} at ${createResponse.data.html_url}`
+    );
+  } catch (err) {
+    core.setFailed(`Failed to create pull request: ${err}`)
+  }
+}
+
+async function run () {
+  try {
+    updateCharts()
+    await pushChanges()
+    await draftPR()
+  } catch (err) {
+    return core.setFailed(`Error: ${err}`)
+  }
+}
+
+run()
